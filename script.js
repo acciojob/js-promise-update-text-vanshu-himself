c928ff9d5a4d7f0ea92b76c0a1e6bfdc963b5ad1
vanshu-himself <124697864+vanshu-himself@users.noreply.github.com>
2023-05-04 11:51:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 //your JS code here. If required.
-const h = document.getElementbyId("output");
-Promise prom=new Promise(resolve());
-function resolve(){
-	h.innerText("hii");
-}setTimeout(resolve(),1000);
+// const h = document.getElementbyId("output");
+// Promise prom=new Promise(resolve());
+// function resolve(){
+// 	h.innerText("hii");
+// }setTimeout(resolve(),1000);
+
+
+const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(["Hello world!"]);
+        }, 1000);
+    });
+      
+    promise.then(values => {
+    console.log(values);
+    });
