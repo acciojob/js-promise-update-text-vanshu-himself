d05d2b7b7889bf9deab91df89420aa303bca8988
vanshu-himself <124697864+vanshu-himself@users.noreply.github.com>
2023-05-04 11:47:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+const h = document.getElementbyId("output");
+Promise prom=new Promise(resolve());
+function resolve(){
+	h.innerText("hii");
+}setTimeout(resolve(),1000);
